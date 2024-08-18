30fa479d1aaffd2b942a52f9a382782fa4e047ba
malika <shaakerimova.m@gmail.com>
2024-08-18 14:10:46 +0200

    js code revised and minor styling changes

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const fieldDOM=document.getElementById('field')
     const field=[
         [0,0,0,0],
-        [0,1024,0,0],
         [0,0,0,0],
-        [0,0,2,0]
+        [0,0,0,0],
+        [0,0,0,0]
     ]
+    function getRandomIndex(value) {
+        const randomIndex = Math.ceil(Math.random() * value);
+        return randomIndex
+    }
+    function defineTwo() {
+        const randomIndexI = getRandomIndex(field[0].length-1);
+        const randomIndexJ = getRandomIndex(field[0].length-1);
+        if (field[randomIndexI][randomIndexJ]!=2) 
+        {
+            field[randomIndexI][randomIndexJ]=2
+        } else {
+            defineTwo()
+        } 
+        console.log(randomIndexI, randomIndexJ)
+    }
+        defineTwo()
+        defineTwo()    
+
     for (let i=0; i<field.length; i++){
         for (let j=0; j<field[i].length; j++){
             const tileValue=field[i][j]
@@ -17,5 +35,4 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(tileValue)
         }
     }
-    console.log(i,j)
 });
